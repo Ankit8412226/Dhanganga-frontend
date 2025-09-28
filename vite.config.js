fc5d0901f7865446cf8b61a7f5015652acd30f27
Ankit Kumar <ankitpandey841226@gmail.com>
2025-09-28 23:56:22 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(), 
-    react()],
+    tailwindcss(),
+    react()
+  ],
+  preview: {
+    host: true, // bind to 0.0.0.0 so Railway can access it
+    port:  5173, // use Railway's PORT env
+    allowedHosts: [
+      'localhost',
+      'dhanganga-production-adfb.up.railway.app' // Railway domain
+    ]
+  }
 });
